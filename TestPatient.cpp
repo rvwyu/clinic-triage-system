2418390f34567e462b3d1783787911ecf8200038
rvwyu <161073419+rvwyu@users.noreply.github.com>
2025-05-09 21:51:20 -0400

    Update TestPatient.cpp

--- TestPatient.cpp
+++ TestPatient.cpp
@@ -1,14 +1,3 @@
-/*/////////////////////////////////////////////////////////////////////////
-						 OOP244 Ms5:
-Full Name  : Rhowen Vaughn Wendelle Yu
-Student ID#: 142671221
-Email      : rvwyu@myseneca.ca
-Section    : ZEE
-Date       : 2024.04.08
-Authenticity Declaration:
-I have done all the coding by myself and only copied the code that my
-professor provided to complete my workshops and assignments.
-/////////////////////////////////////////////////////////////////////////*/
 #include <iostream>
 #include "TestPatient.h"
 using namespace std;
